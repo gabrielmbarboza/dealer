72bffb5a8ee2ffee7bc4d75004578e0a19b4d6dc
Gabriel Barboza <gabrielmbarboza@gmail.com>
2024-02-25 09:32:10 -0300

    Adds support for reading config.yml

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/gabrielmbarboza/dealer/internal/dealer"
+	"gopkg.in/yaml.v3"
 )
+
+type Service struct {
+	Name      string   `yaml:"name"`
+	Path      string   `yaml:"path"`
+	OriginUrl string   `yaml:"origin_url"`
+	Methods   []string `yaml:"methods"`
+}
+
+type Config struct {
+	Services []Service `yaml:"services"`
+}
 
 func main() {
 	http.HandleFunc("/", handler)
@@ -28,9 +42,35 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	serviceName := strings.Split(uri, "/")[1]
+	servicePath := fmt.Sprintf("/%s", strings.Split(uri, "/")[1])
 
-	if serviceName == "" {
+	if servicePath == "" {
 		log.Fatal("Could not find the service")
 	}
+
+	basePath, _ := os.Getwd()
+
+	configData, err := os.ReadFile(fmt.Sprintf("%s/%s", basePath, "config.yml"))
+	if err != nil {
+		panic(err)
+	}
+
+	config := Config{}
+
+	yaml.Unmarshal(configData, &config)
+
+	service := discoverService(servicePath, config.Services)
+
+	fmt.Println(fmt.Sprintf("%s%s", service.OriginUrl, strings.ReplaceAll(r.URL.Path, service.Path, "")))
+}
+
+func discoverService(path string, services []Service) Service {
+	for _, service := range services {
+		if service.Path == path {
+			fmt.Println(service)
+			return service
+		}
+	}
+
+	return Service{}
 }
